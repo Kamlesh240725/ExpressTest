d07e6d0d809dfbbf8e66f01344129ddc8340dd43
kamleshsuthar <kamleshsuthar240725@gmail.com>
2025-01-11 10:25:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+import "dotenv/config"
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/naacho', (req, res) => {
+  res.send('<h1>Naach mari Bindni</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
